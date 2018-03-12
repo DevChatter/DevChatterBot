504dc0a7e94aa916b52b3da7690524db2304b967
Brendan Enrick <benrick@live.com>
2018-03-12 11:24:10 -0400

    Extract caching layer, add todo

--- src/DevChatter.Bot.Core/Caching/ICacheLayer.cs
+++ src/DevChatter.Bot.Core/Caching/ICacheLayer.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using DevChatter.Bot.Core.Data;
+﻿using DevChatter.Bot.Core.Data;
 
 namespace DevChatter.Bot.Core.Caching
 {
@@ -7,24 +6,5 @@ namespace DevChatter.Bot.Core.Caching
     {
         T TryGet<T>(string cacheKey) where T : DataItem;
         void Insert<T>(T item, string cacheKey) where T : DataItem;
-    }
-
-    public class InMemoryCacheLayer : ICacheLayer
-    {
-        private readonly Dictionary<string, DataItem> _cache = new Dictionary<string, DataItem>();
-
-        public T TryGet<T>(string cacheKey) where T : DataItem
-        {
-            if (_cache.ContainsKey(cacheKey))
-            {
-                return _cache[cacheKey] as T;
-            }
-            return null;
-        }
-
-        public void Insert<T>(T item, string cacheKey) where T : DataItem
-        {
-            _cache[cacheKey] = item;
-        }
     }
 }
