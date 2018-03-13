e32e7e6b7d9937bee54327d880da7685c1584247
Brendan Enrick <benrick@live.com>
2018-03-13 13:20:06 -0400

    Move caching layer

--- src/DevChatter.Bot.Core/Data/Caching/InMemoryCacheLayer.cs
+++ src/DevChatter.Bot.Core/Data/Caching/InMemoryCacheLayer.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using DevChatter.Bot.Core.Model;
 
-namespace DevChatter.Bot.Core.Caching
+namespace DevChatter.Bot.Core.Data.Caching
 {
     // TODO: Create a better ICacheLayer implementation (have this wrap a real one, move to infra)
     public class InMemoryCacheLayer : ICacheLayer
