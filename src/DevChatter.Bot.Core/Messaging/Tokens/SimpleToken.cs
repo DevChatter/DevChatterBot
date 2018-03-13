62169612cdb719121bb15111082e62cef0021e85
Brendan Enrick <benrick@live.com>
2018-03-13 14:57:15 -0400

    Adding SimpleToken Replacements

---
+++ src/DevChatter.Bot.Core/Messaging/Tokens/SimpleToken.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using DevChatter.Bot.Core.Events;
+
+namespace DevChatter.Bot.Core.Messaging.Tokens
+{
+    public class SimpleToken
+    {
+        public static SimpleToken UserDisplayName = new SimpleToken(nameof(UserDisplayName), e => e.ChatUser.DisplayName);
+        public static SimpleToken CommandWord = new SimpleToken(nameof(CommandWord), e => e.CommandWord);
+        public static SimpleToken Arg0 = new SimpleToken(nameof(Arg0), e => e.Arguments?.ElementAtOrDefault(0) ?? "");
+        public static SimpleToken Arg1 = new SimpleToken(nameof(Arg1), e => e.Arguments?.ElementAtOrDefault(1) ?? "");
+        public static SimpleToken Arg2 = new SimpleToken(nameof(Arg2), e => e.Arguments?.ElementAtOrDefault(2) ?? "");
+
+        protected SimpleToken(string replacementString, Func<CommandReceivedEventArgs, string> replacementValueSelector)
+        {
+            _replacementString = replacementString;
+            _replacementValueSelector = replacementValueSelector;
+        }
+
+        protected readonly string _replacementString;
+        protected readonly Func<CommandReceivedEventArgs, string> _replacementValueSelector;
+
+        public string ReplacementToken => $"[{_replacementString}]";
+
+        public string ReplaceCommandValues(string inputText, CommandReceivedEventArgs commandReceivedEventArgs)
+        {
+            return inputText.Replace(ReplacementToken, _replacementValueSelector(commandReceivedEventArgs));
+        }
+    }
+}
