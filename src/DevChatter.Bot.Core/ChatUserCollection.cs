62c9f6391923756090890b8fa631816fe759a2ba
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-03-24 14:03:17 -0400

    locking on _repository and active chatter access

---
+++ src/DevChatter.Bot.Core/ChatUserCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevChatter.Bot.Core.Data;
+using DevChatter.Bot.Core.Model;
+
+namespace DevChatter.Bot.Core
+{
+    public class ChatUserCollection
+    {
+        private readonly IRepository _repository;
+
+        private readonly object _activeChatUsersLock = new object();
+        private readonly List<ChatUser> _activeChatUsers = new List<ChatUser>();
+
+        public ChatUserCollection(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool NeedToWatchUser(string displayName)
+        {
+            // Don't lock in here
+            // ReSharper disable once InconsistentlySynchronizedField
+            return _activeChatUsers.All(x => x.DisplayName != displayName);
+        }
+
+
+        public void WatchUser(ChatUser userFromDb)
+        {
+            if (NeedToWatchUser(userFromDb.DisplayName))
+            {
+                lock (_activeChatUsersLock)
+                {
+                    if (NeedToWatchUser(userFromDb.DisplayName))
+                    {
+                        _activeChatUsers.Add(userFromDb);
+                    }
+                }
+            }
+        }
+
+        public void UpdateEachChatter(Action<ChatUser> updateToApply) => UpdateSpecficChatters(updateToApply, x => true);
+
+        public void UpdateSpecficChatters(Action<ChatUser> updateToApply, Func<ChatUser, bool> filter)
+        {
+            lock (_activeChatUsersLock)
+            {
+                List<ChatUser> usersToUpdate = _activeChatUsers.Where(filter).ToList();
+                foreach (ChatUser chatUser in usersToUpdate)
+                {
+                    updateToApply(chatUser);
+                }
+
+                _repository.Update(usersToUpdate);
+            }
+        }
+
+        public void StopWatching(string displayName)
+        {
+            lock (_activeChatUsersLock)
+            {
+                _activeChatUsers.RemoveAll(x => x.DisplayName == displayName);
+            }
+        }
+    }
+}
