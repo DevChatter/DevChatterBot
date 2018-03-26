2b7b4e52db02fda5185b812c55481822d8fb7761
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-03-26 15:23:11 -0400

    Extract Policy to own file

---
+++ src/DevChatter.Bot.Core/Data/CommandPolicy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using DevChatter.Bot.Core.Commands;
+
+namespace DevChatter.Bot.Core.Data
+{
+    public class CommandPolicy : DataItemPolicy<SimpleCommand>
+    {
+        protected CommandPolicy(Expression<Func<SimpleCommand, bool>> expression) : base(expression)
+        {
+        }
+
+        public static CommandPolicy ByCommandText(string commandText)
+        {
+            return new CommandPolicy(x => x.CommandText == commandText);
+        }
+    }
+}
--- src/DevChatter.Bot.Core/Data/DataItemPolicy.cs
+++ src/DevChatter.Bot.Core/Data/DataItemPolicy.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq.Expressions;
-using DevChatter.Bot.Core.Commands;
 using DevChatter.Bot.Core.Model;
 
 namespace DevChatter.Bot.Core.Data
@@ -19,17 +18,5 @@ namespace DevChatter.Bot.Core.Data
 
         public Expression<Func<T, bool>> Criteria { get; }
         public string CacheKey => $"{typeof(T).Name}-{Criteria}";
-    }
-
-    public class CommandPolicy : DataItemPolicy<SimpleCommand>
-    {
-        protected CommandPolicy(Expression<Func<SimpleCommand, bool>> expression) : base(expression)
-        {
-        }
-
-        public static CommandPolicy ByCommandText(string commandText)
-        {
-            return new CommandPolicy(x => x.CommandText == commandText);
-        }
     }
 }
