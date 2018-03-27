ef8221b202c97a1d4368c1b2df8e634c7e8bcb84
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-03-27 14:21:03 -0400

    fix null reference in checking user coins

--- src/DevChatter.Bot.Core/ChatUserCollection.cs
+++ src/DevChatter.Bot.Core/ChatUserCollection.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using DevChatter.Bot.Core.Data;
 using DevChatter.Bot.Core.Data.Model;
+using DevChatter.Bot.Core.Data.Specifications;
 
 namespace DevChatter.Bot.Core
 {
     public class ChatUserCollection
     {
         private readonly IRepository _repository;
+        private readonly object _userCreationLock = new object();
 
         private readonly object _activeChatUsersLock = new object();
         private readonly List<ChatUser> _activeChatUsers = new List<ChatUser>();
@@ -67,8 +69,24 @@ namespace DevChatter.Bot.Core
         public bool UserHasAtLeast(string username, int tokensToRemove)
         {
             ChatUser chatUser = _activeChatUsers.SingleOrDefault(x => x.DisplayName == username);
+            if (chatUser == null)
+            {
+                chatUser = GetOrCreateChatUser(username, new ChatUser {DisplayName = username});
+                WatchUser(chatUser);
+            }
 
             return chatUser.Tokens >= tokensToRemove;
         }
+
+        public ChatUser GetOrCreateChatUser(string displayName, ChatUser chatUser)
+        {
+            lock (_userCreationLock)
+            {
+                ChatUser userFromDb = _repository.Single(ChatUserPolicy.ByDisplayName(displayName));
+                userFromDb = userFromDb ?? _repository.Create(chatUser);
+                return userFromDb;
+            }
+        }
+
     }
 }
--- src/DevChatter.Bot.Core/Events/CurrencyGenerator.cs
+++ src/DevChatter.Bot.Core/Events/CurrencyGenerator.cs
@@ -1,20 +1,16 @@
 ﻿using System.Collections.Generic;
 using DevChatter.Bot.Core.Data;
 using DevChatter.Bot.Core.Data.Model;
-using DevChatter.Bot.Core.Data.Specifications;
 using DevChatter.Bot.Core.Systems.Chat;
 
 namespace DevChatter.Bot.Core.Events
 {
     public class CurrencyGenerator
     {
-        private readonly object _userCreationLock = new object();
-        private readonly IRepository _repository;
         private readonly ChatUserCollection _chatUserCollection;
 
         public CurrencyGenerator(List<IChatClient> chatClients, IRepository repository)
         {
-            _repository = repository;
             _chatUserCollection = new ChatUserCollection(repository);
             foreach (IChatClient chatClient in chatClients)
             {
@@ -42,24 +38,15 @@ namespace DevChatter.Bot.Core.Events
         {
             if (_chatUserCollection.NeedToWatchUser(displayName))
             {
-                ChatUser userFromDb = GetOrCreateChatUser(displayName, chatUser);
+                ChatUser userFromDb = _chatUserCollection.GetOrCreateChatUser(displayName, chatUser);
                 _chatUserCollection.WatchUser(userFromDb);
             }
         }
 
-        private ChatUser GetOrCreateChatUser(string displayName, ChatUser chatUser)
-        {
-            lock (_userCreationLock)
-            {
-                ChatUser userFromDb = _repository.Single(ChatUserPolicy.ByDisplayName(displayName));
-                userFromDb = userFromDb ?? _repository.Create(chatUser);
-                return userFromDb;
-            }
-        }
 
         private void ChatClientOnUserLeft(object sender, UserStatusEventArgs eventArgs)
         {
-            GetOrCreateChatUser(eventArgs.DisplayName, eventArgs.ToChatUser());
+            _chatUserCollection.GetOrCreateChatUser(eventArgs.DisplayName, eventArgs.ToChatUser());
 
             _chatUserCollection.StopWatching(eventArgs.DisplayName);
         }
