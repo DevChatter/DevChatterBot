14c551c5ea9066facde8e4047f38e748d5cf296f
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-12 14:39:38 -0400

    Pull Command management into operations

--- src/DevChatter.Bot.Core/Commands/CommandsCommand.cs
+++ src/DevChatter.Bot.Core/Commands/CommandsCommand.cs
@@ -1,9 +1,8 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using DevChatter.Bot.Core.Commands.Operations;
 using DevChatter.Bot.Core.Data;
 using DevChatter.Bot.Core.Data.Model;
-using DevChatter.Bot.Core.Data.Specifications;
 using DevChatter.Bot.Core.Events.Args;
 using DevChatter.Bot.Core.Systems.Chat;
 
@@ -11,6 +10,14 @@ namespace DevChatter.Bot.Core.Commands
 {
     public class CommandsCommand : BaseCommand
     {
+        private List<ICommandOperation> _operations;
+
+        public List<ICommandOperation> Operations => _operations ?? (_operations = new List<ICommandOperation>
+        {
+            new AddCommandOperation(Repository, AllCommands),
+            new DeleteCommandOperation(Repository, AllCommands)
+        });
+
         public CommandsCommand(IRepository repository)
             : base(repository, UserRole.Everyone)
         {
@@ -21,21 +28,16 @@ namespace DevChatter.Bot.Core.Commands
         public override void Process(IChatClient chatClient, CommandReceivedEventArgs eventArgs)
         {
             string oper = eventArgs?.Arguments?.ElementAtOrDefault(0);
-            string commandWord = eventArgs?.Arguments?.ElementAtOrDefault(1);
-            string staticResponse = eventArgs?.Arguments?.ElementAtOrDefault(2);
-            string roleRequired = eventArgs?.Arguments?.ElementAtOrDefault(3);
 
-            switch (oper)
+            var operationToUse = Operations.SingleOrDefault(x => x.ShouldExecute(oper));
+            if (operationToUse != null)
             {
-                case "add":
-                    AddCommand(chatClient, eventArgs?.ChatUser, commandWord, staticResponse, roleRequired);
-                    break;
-                case "del":
-                    DelCommand(chatClient, eventArgs?.ChatUser, commandWord);
-                    break;
-                default:
-                    ShowAllCommands(chatClient, eventArgs?.ChatUser);
-                    break;
+                string resultMessage = operationToUse.TryToExecute(eventArgs);
+                chatClient.SendMessage(resultMessage);
+            }
+            else
+            {
+                ShowAllCommands(chatClient, eventArgs?.ChatUser);
             }
         }
 
@@ -47,75 +49,6 @@ namespace DevChatter.Bot.Core.Commands
             string stringOfCommands = string.Join(", ", listOfCommands);
             chatClient.SendMessage(
                 $"These are the commands that {eventArgsChatUser.DisplayName} is allowed to run: ({stringOfCommands})");
-        }
-
-        private void DelCommand(IChatClient chatClient, ChatUser chatUser, string commandWord)
-        {
-            try
-            {
-                if (!chatUser.CanUserRunCommand(UserRole.Mod))
-                {
-                    chatClient.SendMessage("You need to be a moderator to delete a command.");
-                    return;
-                }
-
-                SimpleCommand command = Repository.Single(CommandPolicy.ByCommandText(commandWord));
-                if (command == null)
-                {
-                    chatClient.SendMessage($"I didn't find a !{commandWord} command.");
-                    return;
-                }
-
-                chatClient.SendMessage($"Removing the !{commandWord} command.");
-
-                Repository.Remove(command);
-                IBotCommand botCommand = AllCommands.SingleOrDefault(x => x.ShouldExecute(commandWord));
-                if (botCommand != null)
-                {
-                    AllCommands.Remove(botCommand);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-
-        private void AddCommand(IChatClient chatClient, ChatUser chatUser, string commandWord,
-            string staticResponse, string roleText)
-        {
-            try
-            {
-                if (!chatUser.CanUserRunCommand(UserRole.Mod))
-                {
-                    chatClient.SendMessage("You need to be a moderator to add a command.");
-                    return;
-                }
-
-                // !AddCommand Twitter "https://twitter.com/DevChatter_" Everyone
-                if (!Enum.TryParse(roleText, true, out UserRole role))
-                {
-                    role = UserRole.Everyone;
-                }
-
-                SimpleCommand command = new SimpleCommand(commandWord, staticResponse, role);
-
-                if (Repository.Single(CommandPolicy.ByCommandText(command.CommandText)) != null)
-                {
-                    chatClient.SendMessage($"There's already a command using !{command.CommandText}");
-                    return;
-                }
-
-                chatClient.SendMessage(
-                    $"Adding a !{command.CommandText} command for {command.RoleRequired}. It will respond with {command.StaticResponse}.");
-
-                Repository.Create(command);
-                AllCommands.Add(command);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
         }
     }
 }
---
+++ src/DevChatter.Bot.Core/Commands/Operations/DeleteCommandOperation.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevChatter.Bot.Core.Data;
+using DevChatter.Bot.Core.Data.Model;
+using DevChatter.Bot.Core.Data.Specifications;
+using DevChatter.Bot.Core.Events.Args;
+
+namespace DevChatter.Bot.Core.Commands.Operations
+{
+    public class DeleteCommandOperation : BaseCommandOperation
+    {
+        private readonly IRepository _repository;
+        private readonly IList<IBotCommand> _allCommands;
+
+        public DeleteCommandOperation(IRepository repository, IList<IBotCommand> allCommands)
+        {
+            _repository = repository;
+            _allCommands = allCommands;
+        }
+
+        public override List<string> OperandWords { get; } = new List<string> { "del", "rem", "remove", "delete" };
+
+        public override string TryToExecute(CommandReceivedEventArgs eventArgs)
+        {
+            string commandWord = eventArgs?.Arguments?.ElementAtOrDefault(1);
+
+            ChatUser chatUser = eventArgs.ChatUser;
+            try
+            {
+                if (!chatUser.CanUserRunCommand(UserRole.Mod))
+                {
+                    return "You need to be a moderator to delete a command.";
+                }
+
+                SimpleCommand command = _repository.Single(CommandPolicy.ByCommandText(commandWord));
+                if (command == null)
+                {
+                    return $"I didn't find a !{commandWord} command.";
+                }
+
+                IBotCommand botCommand = _allCommands.SingleOrDefault(x => x.ShouldExecute(commandWord));
+                if (botCommand != null)
+                {
+                    _allCommands.Remove(botCommand);
+                }
+                _repository.Remove(command);
+                return $"Removing the !{commandWord} command.";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return "";
+            }
+        }
+    }
+}
