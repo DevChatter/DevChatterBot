666fa61acfee5ec4b45cc2cf46210ac144e15761
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-04-14 15:50:38 -0400

    JoinGameResult Equals method

--- src/DevChatter.Bot.Core/Games/Heist/JoinGameResult.cs
+++ src/DevChatter.Bot.Core/Games/Heist/JoinGameResult.cs
@@ -1,7 +1,28 @@
-﻿namespace DevChatter.Bot.Core.Games.Heist
+namespace DevChatter.Bot.Core.Games.Heist
 {
     public class JoinGameResult
     {
+        protected bool Equals(JoinGameResult other)
+        {
+            return Success == other.Success && string.Equals(Message, other.Message);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((JoinGameResult) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Success.GetHashCode() * 397) ^ (Message != null ? Message.GetHashCode() : 0);
+            }
+        }
+
         public JoinGameResult(bool success, string message)
         {
             Success = success;
