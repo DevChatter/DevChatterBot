bb1e4f1b179ba75a653b7232210600227bd1f365
Johan Bennink <j.bennink@gameswap.nl>
2018-04-26 22:32:07 +0200

    Added the HangmanWord to EF and created the initial seeding.

---
+++ src/DevChatter.Bot.Core/Data/Model/HangmanWord.cs
@@ -0,0 +1,17 @@
+using System;
+namespace DevChatter.Bot.Core.Data.Model
+{
+    public class HangmanWord : DataEntity
+    {
+        public HangmanWord()
+        {
+        }
+
+        public HangmanWord(string word)
+        {
+            Word = word;
+        }
+
+        public string Word { get; protected set; }
+    }
+}
--- src/DevChatter.Bot.Infra.Ef/Migrations/20180426202937_HangmanWords.Designer.cs
+++ src/DevChatter.Bot.Infra.Ef/Migrations/20180426202937_HangmanWords.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace DevChatter.Bot.Infra.Ef.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    partial class AppDataContextModelSnapshot : ModelSnapshot
+    [Migration("20180426202937_HangmanWords")]
+    partial class HangmanWords
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
