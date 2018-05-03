2d9280a8470c178fe1fe5128ce110ba641b24616
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-03 13:38:47 -0400

    remove unused field from AutoActionSystem

--- src/DevChatter.Bot.Core/Automation/AutomatedActionSystem.cs
+++ src/DevChatter.Bot.Core/Automation/AutomatedActionSystem.cs
@@ -1,22 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using DevChatter.Bot.Core.Data.Model;
-using DevChatter.Bot.Core.Messaging;
-using DevChatter.Bot.Core.Systems.Chat;
-using DevChatter.Bot.Core.Util;
 
 namespace DevChatter.Bot.Core.Automation
 {
     public class AutomatedActionSystem : IAutomatedActionSystem
     {
         private readonly IList<IIntervalAction> _actions;
-        private readonly IClock _clock;
 
-        public AutomatedActionSystem(IList<IIntervalAction> actions, IClock clock)
+        public AutomatedActionSystem(IList<IIntervalAction> actions)
         {
             _actions = actions;
-            _clock = clock;
         }
 
         public void RunNecessaryActions()
