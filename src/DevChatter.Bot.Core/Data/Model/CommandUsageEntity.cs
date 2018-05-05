12ab7c6ddd0584867c22d516800df5acf1accc3d
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-05 14:24:57 -0400

    Use ctor for CommandUsageEntity

--- src/DevChatter.Bot.Core/Data/Model/CommandUsageEntity.cs
+++ src/DevChatter.Bot.Core/Data/Model/CommandUsageEntity.cs
@@ -4,6 +4,19 @@ namespace DevChatter.Bot.Core.Data.Model
 {
     public class CommandUsageEntity : DataEntity
     {
+        protected CommandUsageEntity()
+        {
+        }
+
+        public CommandUsageEntity(string commandWord, string fullTypeName,
+            string userId, string userDisplayName)
+        {
+            CommandWord = commandWord;
+            FullTypeName = fullTypeName;
+            UserId = userId;
+            UserDisplayName = userDisplayName;
+        }
+
         public string CommandWord { get; set; }
         public string FullTypeName { get; set; }
         public string UserId { get; set; }
