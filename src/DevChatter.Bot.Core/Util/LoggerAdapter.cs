ff1211f39ddbd11ca8b85d337cda839dfe852f65
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-14 15:25:02 -0400

    Adding in the logger interface and implementation

---
+++ src/DevChatter.Bot.Core/Util/LoggerAdapter.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace DevChatter.Bot.Core.Util
+{
+    public class LoggerAdapter<T> : ILoggerAdapter<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogError(Exception ex, string message, params object[] args)
+        {
+            _logger.LogError(ex, message, args);
+        }
+
+        public void LogInformation(string message)
+        {
+            _logger.LogInformation(message);
+        }
+    }
+}
