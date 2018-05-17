5cc11c8cdbbc504b3244140f9e29403d9ad397ad
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-17 14:03:24 -0400

    Adding scaffold for QuizQuestions

---
+++ src/DevChatter.Bot.Web/Pages/QuizQuestions/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DevChatter.Bot.Core.Data.Model;
+using DevChatter.Bot.Infra.Ef;
+
+namespace DevChatter.Bot.Web.Pages.QuizQuestions
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly DevChatter.Bot.Infra.Ef.AppDataContext _context;
+
+        public DetailsModel(DevChatter.Bot.Infra.Ef.AppDataContext context)
+        {
+            _context = context;
+        }
+
+        public QuizQuestion QuizQuestion { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            QuizQuestion = await _context.QuizQuestions.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (QuizQuestion == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
