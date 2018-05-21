cbb5d095df10716cadec5e126d2514359b7d8c12
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-05-21 14:43:57 -0400

    Adding a mockup for the games managing page

---
+++ src/DevChatter.Bot.Web/Pages/Games/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevChatter.Bot.Web.Pages.Games
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
