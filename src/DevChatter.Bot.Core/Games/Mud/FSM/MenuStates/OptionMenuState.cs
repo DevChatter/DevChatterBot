12b7a936f0c596c2f41a5bd7c631349bdfe37ae9
Tommy_TwoToes <tommy.kristiansen@gmail.com>
2018-05-24 22:53:47 +0200

    FSM options
    Options boilerplate for configuring... options.
    
    Such as sound if wanted and controls or something.

---
+++ src/DevChatter.Bot.Core/Games/Mud/FSM/MenuStates/OptionMenuState.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace DevChatter.Bot.Core.Games.Mud.FSM.MenuStates
+{
+    public class OptionsState : State
+    {
+        public OptionsState(string name) : base(name)
+        {
+        }
+
+        public override bool Run()
+        {
+            bool run = true;
+
+            Menu m = new Menu(new string[] { "Sound", "Controls", "Back" });
+            var k = m.PrintMenuInt();
+            switch (k)
+            {
+                case 0:
+                    StateMachine.MenuInstance.AddState(new SoundOptionState("Sound")); // play sounds in stream?
+                    break;
+
+                case 1:
+                    StateMachine.MenuInstance.AddState(new ControlOptionState("Controls"));
+                    break;
+
+                case 2:
+                    StateMachine.MenuInstance.RemoveState();
+
+                    break;
+
+                default:
+                    break;
+            }
+            return run;
+        }
+
+        public override void Enter()
+        {
+            Console.Clear();
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+}
---
+++ src/DevChatter.Bot.Core/Games/Mud/FSM/MenuStates/SoundOptionState.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace DevChatter.Bot.Core.Games.Mud.FSM.MenuStates
+{
+    public class SoundOptionState : State
+    {
+        public SoundOptionState(string name) : base(name)
+        {
+        }
+
+        public override void Enter()
+        {
+            Console.Clear();
+            Console.WriteLine("Sound options:");
+            Console.WriteLine("Do you want beeps?");
+            CharacterInfo.SoundEnabled = Menu.YesNo();
+        }
+
+        public override void Exit()
+        {
+        }
+
+        public override bool Run()
+        {
+            bool run = true;
+
+            Menu m = new Menu(new string[] { "Back" });
+            var k = m.PrintMenuInt();
+            switch (k)
+            {
+                case 0:
+                    StateMachine.MenuInstance.RemoveState();
+                    break;
+
+                default:
+                    break;
+            }
+            return run;
+        }
+    }
+}
