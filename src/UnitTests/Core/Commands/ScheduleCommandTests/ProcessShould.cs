7a24ff2f8468871bc2915b5cc610cbe06bd71a13
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-06-16 15:54:30 -0400

    Avoid magic string to fix test

--- src/UnitTests/Core/Commands/ScheduleCommandTests/ProcessShould.cs
+++ src/UnitTests/Core/Commands/ScheduleCommandTests/ProcessShould.cs
@@ -1,21 +1,35 @@
+using System;
 using DevChatter.Bot.Core.Commands;
 using DevChatter.Bot.Core.Data;
-using DevChatter.Bot.Core.Events;
 using DevChatter.Bot.Core.Events.Args;
 using DevChatter.Bot.Core.Systems.Chat;
 using Moq;
 using System.Collections.Generic;
-using UnitTests.Fakes;
+using DevChatter.Bot.Core.Data.Model;
+using DevChatter.Bot.Core.Data.Specifications;
 using Xunit;
 
 namespace UnitTests.Core.Commands.ScheduleCommandTests
 {
     public class ScheduleCommandShould
     {
-        private ScheduleCommand _scheduleCommand;
+        private readonly ScheduleCommand _scheduleCommand;
         private readonly Mock<IChatClient> _chatClientMock = new Mock<IChatClient>();
         private readonly Mock<IRepository> _repositoryMock = new Mock<IRepository>();
-        private CommandReceivedEventArgs commandReceivedEventArgs = new CommandReceivedEventArgs();
+        private readonly CommandReceivedEventArgs _commandReceivedEventArgs = new CommandReceivedEventArgs();
+
+        public ScheduleCommandShould()
+        {
+            var entities = new List<ScheduleEntity>
+            {
+                new ScheduleEntity { ExampleDateTime = new DateTimeOffset(2018,6,18,18, 0, 0, 0, TimeSpan.Zero)},
+                new ScheduleEntity { ExampleDateTime = new DateTimeOffset(2018,6,19,18, 0, 0, 0, TimeSpan.Zero)},
+                new ScheduleEntity { ExampleDateTime = new DateTimeOffset(2018,6,21,16, 0, 0, 0, TimeSpan.Zero)},
+                new ScheduleEntity { ExampleDateTime = new DateTimeOffset(2018,6,23,17, 0, 0, 0, TimeSpan.Zero)},
+            };
+            _repositoryMock.Setup(x => x.List(It.IsAny<DataItemPolicy<ScheduleEntity>>())).Returns(entities);
+            _scheduleCommand = new ScheduleCommand(_repositoryMock.Object);
+        }
 
         [Theory]
         [InlineData("-19")]
@@ -23,30 +37,27 @@ namespace UnitTests.Core.Commands.ScheduleCommandTests
         [InlineData("stoptryingtobreakstuff")]
         public void SendErrorMessage_GivenInvalidArguments(string argument)
         {
-            _scheduleCommand = new ScheduleCommand(_repositoryMock.Object);
             List<string> arguments = new List<string>
             {
                 argument
             };
-            commandReceivedEventArgs.Arguments = arguments;
-            _scheduleCommand.Process(_chatClientMock.Object, commandReceivedEventArgs);
-            _chatClientMock.Verify(x => x.SendMessage("UTC offset must be a whole number between -18 and +19"));
+            _commandReceivedEventArgs.Arguments = arguments;
+            _scheduleCommand.Process(_chatClientMock.Object, _commandReceivedEventArgs);
+            _chatClientMock.Verify(x => x.SendMessage(ScheduleCommand.Messages.OUT_OF_RANGE));
         }
 
-        // 
         [Theory]
         [InlineData("", "Our usual schedule (at UTC +0) is: Mondays at 6:00 PM, Tuesdays at 6:00 PM, Thursdays at 4:00 PM, Saturdays at 5:00 PM")]
         [InlineData("-4", "Our usual schedule (at UTC -4) is: Mondays at 2:00 PM, Tuesdays at 2:00 PM, Thursdays at 12:00 PM, Saturdays at 1:00 PM")]
         [InlineData("+8", "Our usual schedule (at UTC +8) is: Tuesdays at 2:00 AM, Wednesdays at 2:00 AM, Fridays at 12:00 AM, Sundays at 1:00 AM")]
         public void SendMatchingScheduleMessage_GivenValidArguments(string argument, string expectedMessage)
         {
-            _scheduleCommand = new ScheduleCommand(_repositoryMock.Object);
             List<string> arguments = new List<string>
             {
                 argument
             };
-            commandReceivedEventArgs.Arguments = arguments;
-            _scheduleCommand.Process(_chatClientMock.Object, commandReceivedEventArgs);
+            _commandReceivedEventArgs.Arguments = arguments;
+            _scheduleCommand.Process(_chatClientMock.Object, _commandReceivedEventArgs);
             _chatClientMock.Verify(x => x.SendMessage(expectedMessage));
         }
 
@@ -57,7 +68,6 @@ namespace UnitTests.Core.Commands.ScheduleCommandTests
         //// Abbreviations taken from https://en.wikipedia.org/wiki/List_of_time_zone_abbreviations
         //public void SendMatchingScheduleMessage_GivenValidTimeZoneAbbreviation(string argument, string expectedMessage)
         //{
-        //    _scheduleCommand = new ScheduleCommand(_repositoryMock.Object);
         //    List<string> arguments = new List<string>
         //    {
         //        argument
@@ -75,7 +85,6 @@ namespace UnitTests.Core.Commands.ScheduleCommandTests
         // Cities taken from https://en.wikipedia.org/wiki/List_of_UTC_time_offsets
         //public void SendMatchingScheduleMessage_GivenPrincipalCity(string argument, string expectedMessage)
         //{
-        //    _scheduleCommand = new ScheduleCommand(_repositoryMock.Object);
         //    List<string> arguments = new List<string>
         //    {
         //        argument
