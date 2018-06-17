80860ed85ab0defb81cf5ce958a839dc978a2eb9
asyncawake <asyncawake@gmail.com>
2018-06-17 13:32:36 +0200

    Accept Changes by Benrick and update commented-out tests accordingly.

--- src/UnitTests/Core/Commands/ScheduleCommandTests/ProcessShould.cs
+++ src/UnitTests/Core/Commands/ScheduleCommandTests/ProcessShould.cs
@@ -62,9 +62,9 @@ namespace UnitTests.Core.Commands.ScheduleCommandTests
         }
 
         //[Theory]
-        //[InlineData("GMT", "Our usual schedule (at UTC +0) is: Mondays at 6:00 PM, Tuesdays at 6:00 PM, Thursdays at 4:00 PM, Saturdays at 5:00 PM")]
-        //[InlineData("EDT", "Our usual schedule (at UTC -4) is: Mondays at 2:00 PM, Tuesdays at 2:00 PM, Thursdays at 12:00 PM, Saturdays at 1:00 PM")]
-        //[InlineData("CST", "Our usual schedule (at UTC +8) is: Tuesdays at 2:00 AM, Wednesdays at 2:00 AM, Fridays at 12:00 AM, Sundays at 1:00 AM")]
+        //[InlineData("GMT", "Our usual schedule (at UTC +0) is: Tuesdays at 6:00 PM, Thursdays at 4:00 PM")]
+        //[InlineData("EDT", "Our usual schedule (at UTC -4) is: Tuesdays at 2:00 PM, Thursdays at 12:00 PM")]
+        //[InlineData("CST", "Our usual schedule (at UTC +8) is: Wednesdays at 2:00 AM, Fridays at 12:00 AM")]
         //// Abbreviations taken from https://en.wikipedia.org/wiki/List_of_time_zone_abbreviations
         //public void SendMatchingScheduleMessage_GivenValidTimeZoneAbbreviation(string argument, string expectedMessage)
         //{
@@ -72,25 +72,25 @@ namespace UnitTests.Core.Commands.ScheduleCommandTests
         //    {
         //        argument
         //    };
-        //    commandReceivedEventArgs.Arguments = arguments;
-        //    _scheduleCommand.Process(_chatClientMock.Object, commandReceivedEventArgs);
+        //    _commandReceivedEventArgs.Arguments = arguments;
+        //    _scheduleCommand.Process(_chatClientMock.Object, _commandReceivedEventArgs);
         //    _chatClientMock.Verify(x => x.SendMessage(expectedMessage));
         //}
 
         //// Please beware that cities might adjust for daylight savings time and break the test.
         //[Theory]
-        //[InlineData("London", "Our usual schedule (at UTC +0) is: Mondays at 6:00 PM, Tuesdays at 6:00 PM, Thursdays at 4:00 PM, Saturdays at 5:00 PM")]
-        //[InlineData("Halifax", "Our usual schedule (at UTC -4) is: Mondays at 2:00 PM, Tuesdays at 2:00 PM, Thursdays at 12:00 PM, Saturdays at 1:00 PM")]
-        //[InlineData("Singapore", "Our usual schedule (at UTC +8) is: Tuesdays at 2:00 AM, Wednesdays at 2:00 AM, Fridays at 12:00 AM, Sundays at 1:00 AM")]
-        // Cities taken from https://en.wikipedia.org/wiki/List_of_UTC_time_offsets
+        //[InlineData("London", "Our usual schedule (at UTC +0) is: Tuesdays at 6:00 PM, Thursdays at 4:00 PM")]
+        //[InlineData("Halifax", "Our usual schedule (at UTC -4) is: Tuesdays at 2:00 PM, Thursdays at 12:00 PM")]
+        //[InlineData("Singapore", "Our usual schedule (at UTC +8) is: Wednesdays at 2:00 AM, Fridays at 12:00 AM")]
+        //// Cities taken from https://en.wikipedia.org/wiki/List_of_UTC_time_offsets
         //public void SendMatchingScheduleMessage_GivenPrincipalCity(string argument, string expectedMessage)
         //{
         //    List<string> arguments = new List<string>
         //    {
         //        argument
         //    };
-        //    commandReceivedEventArgs.Arguments = arguments;
-        //    _scheduleCommand.Process(_chatClientMock.Object, commandReceivedEventArgs);
+        //    _commandReceivedEventArgs.Arguments = arguments;
+        //    _scheduleCommand.Process(_chatClientMock.Object, _commandReceivedEventArgs);
         //    _chatClientMock.Verify(x => x.SendMessage(expectedMessage));
         //}
 
