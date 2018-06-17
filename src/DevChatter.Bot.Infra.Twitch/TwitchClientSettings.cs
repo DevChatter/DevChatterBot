43dab94f031f1881106034a4f415011f9b07fe65
Siddharth Kochar <sidkcr@gmail.com>
2018-06-17 16:36:02 +0530

    Rename TwitchChannelID to TwitchChannelId

--- src/DevChatter.Bot.Infra.Twitch/TwitchClientSettings.cs
+++ src/DevChatter.Bot.Infra.Twitch/TwitchClientSettings.cs
@@ -3,7 +3,7 @@ namespace DevChatter.Bot.Infra.Twitch
     public class TwitchClientSettings
     {
         public string TwitchUsername { get; set; }
-        public string TwitchChannelID { get; set; }
+        public string TwitchChannelId { get; set; }
         public string TwitchBotUserID { get; set; }
         public string TwitchBotOAuth { get; set; }
         public string TwitchChannelOAuth { get; set; }
