e297c4bfb0f4488796782bc937698861b7a32a25
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-07-26 13:46:04 -0400

    Adding the Overlay Page

---
+++ src/DevChatter.Bot.Web/Pages/Overlay.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevChatter.Bot.Web.Pages
+{
+    public class OverlayModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
