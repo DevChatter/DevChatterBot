749667b41d737a2d6e5ca006b07901da2299f74a
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-07-26 14:18:45 -0400

    Code clean up

--- src/DevChatter.Bot.Web/Pages/Overlay.cshtml.cs
+++ src/DevChatter.Bot.Web/Pages/Overlay.cshtml.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DevChatter.Bot.Web.Pages
