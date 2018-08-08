60648ad7bd5548f57ab0152e5131dfcfbcc1f0c4
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-08-08 15:44:32 -0400

    modularize the overlay file

--- src/DevChatter.Bot.Web/wwwroot/js/overlay.js
+++ src/DevChatter.Bot.Web/wwwroot/js/overlay.js
@@ -1,48 +1,53 @@
-var sprites = [];
+var overlay = (function () {
+  var sprites = [];
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function doHype() {
-  for (var blastIndex = 0; blastIndex < 40; blastIndex++) {
-    for (var i = 0; i < 25; i++) {
-      var hypeSprite = new Sprite('/images/DevchaHypeEmote.png');
-      sprites.push(hypeSprite);
-    }
-    await sleep(250);
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
-}
 
-window.onload = function () {
-
-  var connection = new signalR.HubConnectionBuilder()
-    .withUrl("/BotHub")
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
-
-  connection.start().catch(err => console.error(err.toString()));
-
-  connection.on("Hype", () => {
-    doHype();
-  });
-
-  var canvas = document.getElementById('myCanvas');
-  var ctx = canvas.getContext('2d');
-
-  function render() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    sprites.forEach(function (sprite, i) {
-      sprite.update();
-      sprite.render(ctx);
-      if (sprite.timesRendered > 300) {
-        sprites.splice(i, 1);
+  var doHype = async function() {
+    for (var blastIndex = 0; blastIndex < 20; blastIndex++) {
+      for (var i = 0; i < 25; i++) {
+        var hypeSprite = new Sprite('/images/DevchaHypeEmote.png');
+        sprites.push(hypeSprite);
       }
-    });
-    window.requestAnimationFrame(render);
+      await sleep(250);
+    }
   }
 
-  window.requestAnimationFrame(render);
+  window.onload = function() {
 
-}
+    var connection = new signalR.HubConnectionBuilder()
+      .withUrl("/BotHub")
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    connection.start().catch(err => console.error(err.toString()));
+
+    connection.on("Hype", () => {
+      doHype();
+    });
+
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+
+    function render() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      sprites.forEach(function (sprite, i) {
+        sprite.update();
+        sprite.render(ctx);
+        if (sprite.timesRendered > 300) {
+          sprites.splice(i, 1);
+        }
+      });
+      window.requestAnimationFrame(render);
+    }
+
+    window.requestAnimationFrame(render);
+
+  }
+  return {
+    doHype: doHype
+  };
+}());
 
