4accfa5700aca6a89b5331cce0cd7b315f9cedc1
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-08-19 18:11:46 -0400

    Change Actions to objects

--- src/DevChatter.Bot.Games.Mud/Actions.cs
+++ src/DevChatter.Bot.Games.Mud/Actions.cs
@@ -1,11 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DevChatter.Bot.Games.Mud
 {
-    public enum Actions
+    public class Actions
     {
-        Look,
-        Take,
-        Drop,
-        Attack,
-        Hide
+        public static Actions Look = new Actions(1, nameof(Look), "check", "look at", "examine");
+        public static Actions Take = new Actions(2, nameof(Take), "get", "pick up", "grab");
+        public static Actions Drop = new Actions(3, nameof(Drop), "discard", "trash", "leave");
+        public static Actions Attack = new Actions(4, nameof(Attack), "fight");
+        public static Actions Hide = new Actions(5, nameof(Hide));
+
+        private Actions(int id, params string[] aliases)
+        {
+            Id = id;
+            _aliases = new List<string>(aliases);
+        }
+
+        public int Id { get; }
+        private readonly List<string> _aliases;
+
+        public string PrimaryWord => _aliases.First();
+
+        public bool IsMatch(string verb)
+        {
+            return _aliases.Contains(verb);
+        }
     }
 }
