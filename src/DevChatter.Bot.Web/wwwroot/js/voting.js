c1db0132f872eb9f02d73b3d4584d89f9dea204c
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-01 15:31:22 -0400

    WIP - size rectangle based on text length

--- src/DevChatter.Bot.Web/wwwroot/js/voting.js
+++ src/DevChatter.Bot.Web/wwwroot/js/voting.js
@@ -10,13 +10,15 @@ var voting = (function () {
     isRunning = true;
     ctx.fillStyle = "#cbcbcb";
     var height = 20 + (40 * choices.length);
-    ctx.fillRect(0, 0, 200, height);
+    ctx.font = "30px Arial";
+    // TODO: find the longest measureText result from the choices
+    var textLength = ctx.measureText("TheLongestChoiceInHere");
+    ctx.fillRect(0, 0, textLength.width + 20, height);
     displayChoices(ctx, choices);
   };
 
 
   function displayChoices(ctx, choices) {
-    ctx.font = "30px Arial";
     ctx.fillStyle = "#000000";
     for (var i = 0; i < choices.length; i++) {
       ctx.fillText(choices[i], 10, 40 + 40*i);
