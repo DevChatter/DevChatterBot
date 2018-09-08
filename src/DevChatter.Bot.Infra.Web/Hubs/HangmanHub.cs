5bea9b494ae832173b0dcdc4d0f9292a1f0389da
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-08 15:05:57 -0400

    Remove unnecessary code

--- src/DevChatter.Bot.Infra.Web/Hubs/HangmanHub.cs
+++ src/DevChatter.Bot.Infra.Web/Hubs/HangmanHub.cs
@@ -1,4 +1,3 @@
-using DevChatter.Bot.Core;
 using DevChatter.Bot.Core.Streaming;
 using Microsoft.AspNetCore.SignalR;
 
@@ -6,24 +5,5 @@ namespace DevChatter.Bot.Infra.Web.Hubs
 {
     public class HangmanHub : Hub<IHangmanDisplay>
     {
-        public void HangmanStart()
-        {
-            Clients.All.HangmanStart();
-        }
-
-        public void HangmanWin()
-        {
-            Clients.All.HangmanWin();
-        }
-
-        public void HangmanLose()
-        {
-            Clients.All.HangmanLose();
-        }
-
-        public void HangmanWrongAnswer()
-        {
-            Clients.All.HangmanWrongAnswer();
-        }
     }
 }
--- src/DevChatter.Bot.Infra.Web/Hubs/VotingHub.cs
+++ src/DevChatter.Bot.Infra.Web/Hubs/VotingHub.cs
@@ -1,26 +1,9 @@
-using DevChatter.Bot.Core;
-using DevChatter.Bot.Core.BotModules.VotingModule;
-using Microsoft.AspNetCore.SignalR;
-using System.Collections.Generic;
 using DevChatter.Bot.Core.Streaming;
+using Microsoft.AspNetCore.SignalR;
 
 namespace DevChatter.Bot.Infra.Web.Hubs
 {
     public class VotingHub : Hub<IVotingDisplay>
     {
-        public void VoteStart(IEnumerable<string> choices)
-        {
-            Clients.All.VoteStart(choices);
-        }
-
-        public void VoteReceived(VoteInfoDto voteInfo)
-        {
-            Clients.All.VoteReceived(voteInfo);
-        }
-
-        public void VoteEnd()
-        {
-            Clients.All.VoteEnd();
-        }
     }
 }
