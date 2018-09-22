01894e540a5b94cdb44df6703d73ae843225950f
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-09-22 15:09:50 -0400

    Pull Default Command Data from file

--- src/DevChatter.Bot.Web/Setup/SetUpCommandData.cs
+++ src/DevChatter.Bot.Web/Setup/SetUpCommandData.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using DevChatter.Bot.Core.Commands;
 using DevChatter.Bot.Core.Data;
 using DevChatter.Bot.Core.Data.Model;
 using DevChatter.Bot.Core.Data.Specifications;
+using DevChatter.Bot.Web.DefaultData;
+using Newtonsoft.Json;
 
 namespace DevChatter.Bot.Web.Setup
 {
@@ -42,7 +45,10 @@ namespace DevChatter.Bot.Web.Setup
 
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
-            IEnumerable<TypeInfo> allCommandTypes = assemblies.SelectMany(x => x.DefinedTypes);
+            IEnumerable<TypeInfo> allCommandTypes =
+                assemblies.SelectMany(x => x.DefinedTypes);
+
+            DefaultCommandData commandData = GetDefaultData();
 
             var concreteCommands = allCommandTypes
                 .Where(x => typeof(IBotCommand).IsAssignableFrom(x))
@@ -54,18 +60,31 @@ namespace DevChatter.Bot.Web.Setup
             List<CommandEntity> commandEntities = repository.List(CommandPolicy.All());
             List<string> commandTypes = commandEntities.Select(x => x.FullTypeName).ToList();
 
+            CommandEntity CommandEntityFromTypeAndDefaultData(TypeInfo commandType)
+            {
+                var entity = commandData.Commands.SingleOrDefault(x => x.FullTypeName == commandType.FullName)
+                             ?? new CommandEntity
+                             {
+                                 FullTypeName = commandType.FullName,
+                             };
+                entity.CommandWord = commandType.Name.Substring(0, commandType.Name.Length - conventionSuffix.Length);
+                return entity;
+            }
+
             List<CommandEntity> missingDefaults = concreteCommands
-                .Select(commandType => new CommandEntity
-                {
-                    CommandWord = commandType.Name.Substring(0, commandType.Name.Length - conventionSuffix.Length),
-                    FullTypeName = commandType.FullName,
-                    // TODO: Add the default RoleRequired
-                })
-                .Where(x => !commandTypes.Contains(x.FullTypeName))
+                .Where(x => !commandTypes.Contains(x.FullName))
+                .Select(CommandEntityFromTypeAndDefaultData)
                 .ToList();
 
             var entitiesToRemove = commandEntities.Where(x => concreteCommands.All(c => c.FullName != x.FullTypeName)).ToList();
             return (missingDefaults, entitiesToRemove);
+        }
+
+        private static DefaultCommandData GetDefaultData()
+        {
+            // TODO: Move this to a config file.
+            var rawJson = File.ReadAllText("DefaultData\\DefaultCommandData.json");
+            return JsonConvert.DeserializeObject<DefaultCommandData>(rawJson);
         }
     }
 }
