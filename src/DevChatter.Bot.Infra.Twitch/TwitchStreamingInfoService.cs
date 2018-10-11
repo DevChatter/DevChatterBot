a22b7fd006695d39dc62bac8be80f31d15ec0466
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-10-11 13:31:39 -0400

    Switch StreamInfo to use Helix API

--- src/DevChatter.Bot.Infra.Twitch/TwitchStreamingInfoService.cs
+++ src/DevChatter.Bot.Infra.Twitch/TwitchStreamingInfoService.cs
@@ -1,8 +1,10 @@
-using System;
-using System.Threading.Tasks;
 using DevChatter.Bot.Core.Systems.Streaming;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitchLib.Api.Helix.Models.Streams;
 using TwitchLib.Api.Interfaces;
-using TwitchLib.Api.V5.Models.Streams;
 
 namespace DevChatter.Bot.Infra.Twitch
 {
@@ -10,6 +12,7 @@ namespace DevChatter.Bot.Infra.Twitch
     {
         private readonly ITwitchAPI _twitchApi;
         private readonly TwitchClientSettings _twitchClientSettings;
+        private DateTime? _streamStartedAt;
 
         public TwitchStreamingInfoService(ITwitchAPI twitchApi, TwitchClientSettings twitchClientSettings)
         {
@@ -19,13 +22,20 @@ namespace DevChatter.Bot.Infra.Twitch
 
         public async Task<TimeSpan?> GetUptimeAsync()
         {
-            return await _twitchApi.V5.Streams.GetUptimeAsync(_twitchClientSettings.TwitchChannelId);
+            if (_streamStartedAt == null || _streamStartedAt < DateTime.UtcNow.AddHours(-1))
+            {
+                var userIds = new List<string> { _twitchClientSettings.TwitchChannelId };
+                GetStreamsResponse streamInfo = await _twitchApi.Helix.Streams.GetStreamsAsync(userIds: userIds);
+                _streamStartedAt = streamInfo.Streams.SingleOrDefault()?.StartedAt;
+            }
+            return DateTime.UtcNow - _streamStartedAt;
         }
 
         public async Task<int> GetViewerCountAsync()
         {
-            StreamByUser streamByUserAsync = await _twitchApi.V5.Streams.GetStreamByUserAsync(_twitchClientSettings.TwitchChannelId);
-            return streamByUserAsync.Stream.Viewers;
+            var userIds = new List<string>{ _twitchClientSettings.TwitchChannelId };
+            GetStreamsResponse streamInfo = await _twitchApi.Helix.Streams.GetStreamsAsync(userIds: userIds);
+            return streamInfo.Streams.SingleOrDefault()?.ViewerCount ?? 0;
         }
     }
 }
