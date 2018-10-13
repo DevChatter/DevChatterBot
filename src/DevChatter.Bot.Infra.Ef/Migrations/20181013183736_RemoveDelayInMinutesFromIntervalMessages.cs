82f3a2713aecda628cae11fdf6785ec06dca1279
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-10-13 14:40:46 -0400

    Remove previous message delay column

---
+++ src/DevChatter.Bot.Infra.Ef/Migrations/20181013183736_RemoveDelayInMinutesFromIntervalMessages.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevChatter.Bot.Infra.Ef.Migrations
+{
+    public partial class RemoveDelayInMinutesFromIntervalMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DelayInMinutes",
+                table: "IntervalMessages");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DelayInMinutes",
+                table: "IntervalMessages",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
