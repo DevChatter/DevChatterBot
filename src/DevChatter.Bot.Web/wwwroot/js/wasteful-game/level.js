d4f4788268524b923f9fae0ddef9f55bb35ea103
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-10-25 16:48:23 -0400

    Adding a level concept

---
+++ src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/level.js
@@ -0,0 +1,56 @@
+import { Zombie } from '/js/wasteful-game/zombie.js';
+import { ZombieSpawner } from '/js/wasteful-game/zombie-spawner.js';
+import { Consumable } from '/js/wasteful-game/consumable.js';
+import { ItemEffect } from '/js/wasteful-game/item-effect.js';
+import { Obstacle } from '/js/wasteful-game/obstacle.js';
+
+export class Level {
+  constructor(grid, levelNumber) {
+    this._grid = grid;
+    this._turnNumber = 0;
+
+    this._actors = [];
+    for (let i = 0; i < levelNumber; i++) {
+      this._actors.push(new Zombie(this._grid));
+    }
+
+    this._items = [];
+    // TODO: Random consumable selection
+    let consumable = new Consumable(this._grid, new ItemEffect(1, 5), '/images/ZedChatter/Taco-0.png');
+    this._items.push(consumable);
+
+    for (let i = 0; i < 10; i++) {
+      this._items.push(new Obstacle(this._grid));
+    }
+  }
+
+  update() {
+    this._removeZombies();
+
+    this._actors.forEach(actor => {
+      let spawn = actor.takeTurn();
+      if (spawn) {
+        this._actors.push(spawn);
+      }
+    });
+
+    this._addZombieSpawners();
+
+    this._turnNumber++;
+  }
+
+  _removeZombies() {
+    this._actors = this._actors.filter(zombie => !zombie.isKilled);
+  }
+
+  _addZombieSpawners() {
+    if (this._turnNumber % 6 === 0 && this._turnNumber > 0) {
+      let location = this._grid.getRandomOpenLocation();
+      this._actors.push(new ZombieSpawner(this._grid, location.x, location.y));
+    }
+  }
+
+  get actors() {
+    return this._actors;
+  }
+}
--- src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/wasteful.js
+++ src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/wasteful.js
@@ -1,14 +1,10 @@
-import { Obstacle } from '/js/wasteful-game/obstacle.js';
 import { Info } from '/js/wasteful-game/info.js';
 import { Direction } from '/js/wasteful-game/direction.js';
-import { Zombie } from '/js/wasteful-game/zombie.js';
-import { ZombieSpawner } from '/js/wasteful-game/zombie-spawner.js';
 import { Player } from '/js/wasteful-game/player.js';
-import { Consumable } from '/js/wasteful-game/consumable.js';
 import { Grid } from '/js/wasteful-game/grid.js';
 import { Background } from '/js/wasteful-game/background.js';
-import { ItemEffect } from '/js/wasteful-game/item-effect.js';
 import { ExitTile } from '/js/wasteful-game/exit-tile.js';
+import { Level } from '/js/wasteful-game/level.js';
 
 const wastefulGray = '#cccccc';
 const hangryRed = '#ff0000';
@@ -27,16 +23,12 @@ export class Wasteful {
     if (this._isRunning) return;
 
     this._levelNumber = 1;
-    this._turnNumber = 0;
     this._grid = new Grid(this._canvas, this._context);
     this._info = new Info(this._canvas, this._context, displayName);
     this._exit = new ExitTile(this, this._grid);
     this._player = new Player(this._grid);
-    this._actors = [new Zombie(this._grid)];
-    this._items = [new Consumable(this._grid, new ItemEffect(1,5), '/images/ZedChatter/Taco-0.png')];
-    this._createObstacles();
     this._background = new Background(this._context, this._canvas.width - wastefulInfoWidth, this._canvas.height);
-
+    this._level = new Level(this._grid, this._levelNumber);
     this._isRunning = true;
     window.requestAnimationFrame(() => this._updateFrame());
   }
@@ -44,22 +36,11 @@ export class Wasteful {
   movePlayer(direction) {
     this._player.move(new Direction(direction));
 
-    this._removeZombies();
-
-    this._actors.forEach(actor => {
-      let spawn = actor.takeTurn();
-      if (spawn) {
-        this._actors.push(spawn);
-      }
-    });
+    this._level.update();
 
     if (this._player.health <= 0) {
       this._isGameOver = true;
     }
-
-    this._addZombieSpawners();
-
-    this._turnNumber++;
   }
 
   exitLevel() {
@@ -67,17 +48,12 @@ export class Wasteful {
     let exitLocation = this._exit.location;
     this._player.setNewLocation(0, exitLocation.y);
 
-    this._turnNumber = 0;
-    this._actors = [];
-    this._items = [];
     this._grid.clearSprites();
 
     this._grid.addSprite(this._player);
     this._grid.addSprite(this._exit);
 
-    this._actors.push(new Zombie(this._grid));
-    this._items.push(new Consumable(this._grid, new ItemEffect(1, 5), '/images/ZedChatter/Taco-0.png'));
-    this._createObstacles();
+    this._level = new Level(this._grid, this._levelNumber);
   }
 
   _updateFrame() {
@@ -118,22 +94,5 @@ export class Wasteful {
 
     this._context.fillStyle = wastefulGray;
     this._context.fillRect(this._canvas.width - wastefulInfoWidth, 0, wastefulInfoWidth, this._canvas.height);
-  }
-
-  _createObstacles() {
-    for (let i = 0; i < 10; i++) {
-      this._items.push(new Obstacle(this._grid));
-    }
-  }
-
-  _removeZombies() {
-    this._actors = this._actors.filter(zombie => !zombie.isKilled);
-  }
-
-  _addZombieSpawners() {
-    if (this._turnNumber % 6 === 0 && this._turnNumber > 0) {
-      let location = this._grid.getRandomOpenLocation();
-      this._actors.push(new ZombieSpawner(this._grid, location.x, location.y));
-    }
   }
 }
