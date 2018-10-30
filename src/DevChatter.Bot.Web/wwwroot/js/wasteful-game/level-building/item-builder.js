7f5983d90b2f01298864adfd9aff128b6742874b
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-10-30 15:26:19 -0400

    Refactor item creation

--- src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/level-building/item-builder.js
+++ src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/level-building/item-builder.js
@@ -16,7 +16,7 @@ export class ItemBuilder {
    */
   getItemsForLevel(levelNumber) {
     let itemType = levelNumber % 2 === 0 ? ItemType.CONSUMABLE : ItemType.WEAPON;
-    return [this._pickRandom(this._createStaticData().filter(item => item.type === itemType))];
+    return this._createItemOfType(itemType);
   }
 
   /**
@@ -28,68 +28,24 @@ export class ItemBuilder {
   }
 
   /**
+   * @param ItemType.CONSUMABLE|ItemType.WEAPON
    * @private
    */
-  _createStaticData() {
-    return [
-      new EffectItem(
-        this._game,
-        new Sprite('/images/ZedChatter/Taco-0.png', 1, 1, 1),
-        ItemType.CONSUMABLE,
-        ItemPickupType.INSTANT,
-        [ItemEffectType.HEALTH, ItemEffectType.POINTS],
-        1,
-        [
-          { [ItemEffectType.HEALTH]: 2 },
-          { [ItemEffectType.POINTS]: 5 }
-        ]
-      ),
-      new EffectItem(
-        this._game,
-        new Sprite('/images/ZedChatter/Pizza-0.png', 1, 1, 1),
-        ItemType.CONSUMABLE,
-        ItemPickupType.INSTANT,
-        ItemEffectType.HEALTH,
-        1,
-        [{ [ItemEffectType.HEALTH]: 3 }]
-      ),
-      new EffectItem(
-        this._game,
-        new Sprite('/images/ZedChatter/BlackCan-0.png', 1, 1, 1),
-        ItemType.CONSUMABLE,
-        ItemPickupType.INSTANT,
-        [ItemEffectType.HEALTH, ItemEffectType.POINTS],
-        1,
-        [
-          { [ItemEffectType.HEALTH]: 1 },
-          { [ItemEffectType.POINTS]: 15 }
-        ]
-      ),
-
-      new Item(
-        this._game,
-        new Sprite('/images/ZedChatter/BaseballBat-1.png', 1, 1, 1),
-        ItemType.WEAPON,
-        ItemPickupType.INVENTORY,
-        null,
-        1
-      ),
-      new Item(
-        this._game,
-        new Sprite('/images/ZedChatter/Axe-1.png', 1, 1, 1),
-        ItemType.WEAPON,
-        ItemPickupType.INVENTORY,
-        null,
-        2
-      ),
-      new Item(
-        this._game,
-        new Sprite('/images/ZedChatter/Sword-1.png', 1, 1, 1),
-        ItemType.WEAPON,
-        ItemPickupType.INVENTORY,
-        null,
-        3
-      ),
+  _createItemOfType(itemType) {
+    const items = [
+      new EffectItem(this._game, new Sprite('/images/ZedChatter/Taco-0.png', 1, 1, 1), ItemType.CONSUMABLE, ItemPickupType.INSTANT, [ItemEffectType.HEALTH, ItemEffectType.POINTS], 1, [
+        { [ItemEffectType.HEALTH]: 2 },
+        { [ItemEffectType.POINTS]: 5 }
+      ]),
+      new EffectItem(this._game, new Sprite('/images/ZedChatter/Pizza-0.png', 1, 1, 1), ItemType.CONSUMABLE, ItemPickupType.INSTANT, ItemEffectType.HEALTH, 1, [{ [ItemEffectType.HEALTH]: 3 }]),
+      new EffectItem(this._game, new Sprite('/images/ZedChatter/BlackCan-0.png', 1, 1, 1), ItemType.CONSUMABLE, ItemPickupType.INSTANT, [ItemEffectType.HEALTH, ItemEffectType.POINTS], 1, [
+        { [ItemEffectType.HEALTH]: 1 },
+        { [ItemEffectType.POINTS]: 15 }
+      ]),
+      new Item(this._game, new Sprite('/images/ZedChatter/BaseballBat-1.png', 1, 1, 1), ItemType.WEAPON, ItemPickupType.INVENTORY, null, 1),
+      new Item(this._game, new Sprite('/images/ZedChatter/Axe-1.png', 1, 1, 1), ItemType.WEAPON, ItemPickupType.INVENTORY, null, 2),
+      new Item(this._game, new Sprite('/images/ZedChatter/Sword-1.png', 1, 1, 1), ItemType.WEAPON, ItemPickupType.INVENTORY, null, 3),
     ];
+    return [this._pickRandom(items.filter(item => item.type === itemType))];
   }
 }
