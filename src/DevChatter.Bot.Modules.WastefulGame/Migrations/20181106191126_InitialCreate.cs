b2f8b05a9af83dc1acd6fcfec1bfd7322365afd3
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-11-06 14:12:21 -0500

    Initial Create Migration in EF

---
+++ src/DevChatter.Bot.Modules.WastefulGame/Migrations/20181106191126_InitialCreate.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevChatter.Bot.Modules.WastefulGame.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
