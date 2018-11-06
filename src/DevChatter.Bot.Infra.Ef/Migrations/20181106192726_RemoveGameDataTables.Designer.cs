dd065619361bdcf34fd5545d6b7465b35b74d3a9
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-11-06 14:28:03 -0500

    Remove Game Records Tables from Default DB

--- src/DevChatter.Bot.Infra.Ef/Migrations/20181106192726_RemoveGameDataTables.Designer.cs
+++ src/DevChatter.Bot.Infra.Ef/Migrations/20181106192726_RemoveGameDataTables.Designer.cs
@@ -4,14 +4,16 @@ using DevChatter.Bot.Infra.Ef;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevChatter.Bot.Infra.Ef.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    partial class AppDataContextModelSnapshot : ModelSnapshot
+    [Migration("20181106192726_RemoveGameDataTables")]
+    partial class RemoveGameDataTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
