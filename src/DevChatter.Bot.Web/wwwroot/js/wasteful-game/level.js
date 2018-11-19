18d730b411a39cdc321249bd5330f25ca928ba88
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2018-11-19 15:38:07 -0500

    Adding a house escape possibility

--- src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/level.js
+++ src/DevChatter.Bot.Web/wwwroot/js/wasteful-game/level.js
@@ -1,5 +1,6 @@
 import { SimpleZombie } from '/js/wasteful-game/entity/enemies/simple-zombie.js';
 import { BarrelFire } from '/js/wasteful-game/entity/obstacles/barrel-fire.js';
+import { ExWall } from '/js/wasteful-game/entity/obstacles/ex-wall.js';
 import { AutonomousComponent } from '/js/wasteful-game/entity/components/autonomousComponent.js';
 import { ExitItem } from '/js/wasteful-game/entity/items/exit-item.js';
 import { EscapeItem } from '/js/wasteful-game/entity/items/escape-item.js';
@@ -44,13 +45,7 @@ export class Level {
 
     this._game.entityManager.add(this._player);
 
-    if (this._levelNumber === 3) {
-      this._exit = this._createEscape();
-    } else {
-      this._exit = this._createExit();
-    }
-
-    this._game.entityManager.add(this._exit);
+    this._createLevelExit();
 
     for (let i = 0; i < this._levelNumber; i++) {
       const zombie = new SimpleZombie(this._game);
@@ -70,8 +65,9 @@ export class Level {
       this._game.entityManager.add(barrel);
     }
 
-    let requiredLocation = this._exit.location;
-    let path = this._game.grid.findPath(this._player.location, requiredLocation);
+    let requiredEntity = this._game.entityManager.getFirstByClass(ExitItem)
+      || this._game.entityManager.getFirstByClass(EscapeItem);
+    let path = this._game.grid.findPath(this._player.location, requiredEntity.location);
     if (path.length === 0) {
       // No path to exit exists. Try again.
       this._levelNumber--;
@@ -102,6 +98,18 @@ export class Level {
       const zombie = new SimpleZombie(this._game);
       zombie.setLocation(this._game.grid.getRandomOpenLocation());
       this._game.entityManager.add(zombie);
+    }
+  }
+
+  _createLevelExit() {
+    let randomChance = Math.floor(Math.random() * (90 + this._levelNumber));
+    if (randomChance > 90) {
+      this._game.entityManager.add(this._createEscape());
+    } else if (randomChance > 80) {
+      let houseEntities = this._generateHouseEscapeEntities();
+      houseEntities.forEach(x => this._game.entityManager.add(x));
+    } else {
+      this._game.entityManager.add(this._createExit());
     }
   }
 
@@ -140,5 +148,30 @@ export class Level {
         { [ItemEffectType.POINTS]: 100 }
       ]
     );
+  }
+
+  _generateHouseEscapeEntities() {
+    let doorway = new EscapeItem(
+      this._game,
+      new Sprite(ExWall.blue.door, 1, 1, 1),
+      ItemType.CONSUMABLE,
+      ItemPickupType.INSTANT,
+      ItemEffectType.POINTS,
+      1,
+      [
+        { [ItemEffectType.POINTS]: 100 }
+      ]
+    );
+
+    doorway.setLocation(10,0);
+
+    let houseEntities =
+    [
+      new ExWall(this._game, ExWall.blue.left, 8, 0),
+      new ExWall(this._game, ExWall.blue.window, 9, 0),
+      doorway,
+      new ExWall(this._game, ExWall.blue.right, 11, 0)
+    ];
+    return houseEntities;
   }
 }
